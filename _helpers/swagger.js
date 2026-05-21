3a6ed9d25b57c88a3f3cb6e012fb40226480dc73
Sergio Jr. Neri <gioneri1022@gmail.com>
2026-05-22 00:37:43 +0800

    code typo

--- _helpers/swagger.js
+++ _helpers/swagger.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./swagger.json');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
